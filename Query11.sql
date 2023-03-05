36575907b0c8ccf76502234867e3664dc665cfc9
Sharpy23 <123498009+Sharpy23@users.noreply.github.com>
2023-03-05 17:45:41 +0200

    Create Query11.sql

---
+++ Query11.sql
@@ -0,0 +1,12 @@
+INSERT INTO worker (empid, lastname, firstname, departmentid) 
+VALUES (1, 'Isaid', 'Mohamed', 2); 
+INSERT INTO worker (empid, lastname, firstname, departmentid)
+(9898, 'Alexopoulou', 'Foteini', 2);
+INSERT INTO Assign (projno, empid)
+VALUES (1043, 9898);
+INSERT INTO Assign (projno, empid)
+VALUES (1032, 9898);
+INSERT INTO Assign (projno, empid)
+VALUES (1043, 1);
+INSERT INTO Assign (projno, empid)
+VALUES (1032, 1);
